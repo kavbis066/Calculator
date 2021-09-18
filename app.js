c91238be9ef0726b0439ca64d729c6f09b44b742
Kaveri Biswas <biswaskaveri66@gmail.com>
2021-09-18 16:22:16 +0530

    Added calculation and display functions

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ class Calculator{
     }
 
     delete(){
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
     }
     
     appendNumber(number){
@@ -31,12 +31,57 @@ class Calculator{
     }
 
     compute(){
+        let computation;
+        const previous = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if(isNaN(previous) || isNaN(current)) return;
+        switch(this.operation){
+            case '+':
+                computation = previous + current;
+                break;
+            case '-':
+                computation = previous - current;
+                break;
+            case '*':
+                computation = previous * current;
+                break;
+            case '/':
+                computation = previous / current;
+                break;
+            default:
+                return;
+        }
+        this.currentOperand = computation;
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
 
+    getDisplayNumber(number){
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split('.')[0]);
+        const decimalDigits = stringNumber.split('.')[1];
+        let integerDisplay;
+        if (isNaN(integerDigits)){
+            integerDisplay = '';
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0});
+        }
+        if (decimalDigits != null){
+            return `${integerDisplay}.${decimalDigits}`;
+        } else {
+            return integerDisplay;
+        }
     }
 
     updateDisplay(){
-        this.currentOperandText.innerText = this.currentOperand;
-        this.previousOperandText.innerText = this.previousOperand;
+        this.currentOperandText.innerText = 
+            this.getDisplayNumber(this.currentOperand);
+        if(this.operation != null){
+            this.previousOperandText.innerText = 
+                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
+        } else {
+            this.previousOperandText.innerText = '';
+        }
     }
 }
 
@@ -62,4 +107,19 @@ operationButtons.forEach(button => {
         calculator.selectOperation(button.innerText);
         calculator.updateDisplay();
     })
+})
+
+equalsButton.addEventListener('click', () =>{
+    calculator.compute();
+    calculator.updateDisplay();
+})
+
+allClearButton.addEventListener('click', () =>{
+    calculator.clear();
+    calculator.updateDisplay();
+})
+
+deleteButton.addEventListener('click', () =>{
+    calculator.delete();
+    calculator.updateDisplay();
 })
