bef4a6adee2a85508b5ac019fcfb56941389f116
Kaveri Biswas <biswaskaveri66@gmail.com>
2021-09-18 14:47:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+This is a calculator project built using vanilla javascript. The technologies used are:
+- HTML5
+- CSS
+- JavaScript
+
+Functions performed by this calculator:
+- Addition
+- Subtraction
+- Multiplication
+- Division
+- All Clear: This function will clear all the output or any number entered on the text panel.
+- Delete: This function will let the user delete any wrong digit entered.
---
+++ app.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-numbers]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const previousOperandText = document.querySelector('[data-previous-operand]');
+const currentOperandText = document.querySelector('[data-current-operand]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
